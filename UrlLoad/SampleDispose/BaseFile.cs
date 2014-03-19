c305577e529b175f09f6d5c6ac0f235391cf92da
Alexey Nechunaev <alexey.nechunaev@sibers.com>
2014-03-19 17:24:31 +0700

    Add classes BaseFile and SpecialFile

---
+++ UrlLoad/SampleDispose/BaseFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlLoad.SampleDispose
+{
+    public class BaseFile : IDisposable
+    {
+        protected FileStream fs;
+
+        public BaseFile()
+        {
+            fs = new FileStream(Environment.CurrentDirectory + "test.txt", FileMode.Create, FileAccess.Write);
+        }
+
+        public void WriteBaseInfo()
+        {
+            byte[] array = System.Text.Encoding.Default.GetBytes("It`s base info");
+            fs.Write(array, 0, array.Length);
+        }
+
+        public void Dispose()
+        {
+            fs.Close();
+        }
+    }
+}
---
+++ UrlLoad/SampleDispose/Speсial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlLoad.SampleDispose
+{
+    public class Speсial : BaseFile
+    {
+        public void WriteSpecialInfo()
+        {
+            byte[] array = System.Text.Encoding.Default.GetBytes("It`s special info");
+            fs.Write(array, 0, array.Length);
+        }
+    }
+}
